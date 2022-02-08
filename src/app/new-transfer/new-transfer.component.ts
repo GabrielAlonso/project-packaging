23d42781ce7a3ea68034a7a6c29c4424cca99453
Gabriel Jordão Alonso <gabrieljordaoalonso@gmail.com>
2022-02-08 16:02:37 -0300

    Included method emit to send a value

--- src/app/new-transfer/new-transfer.component.ts
+++ src/app/new-transfer/new-transfer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-new-transfer',
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class newTransferComponent {
 
+  @Output() toTransfer = new EventEmitter<any>();
+
   value_transfer: number;
   destiny_transfer: number;
 
@@ -14,6 +16,7 @@ export class newTransferComponent {
   transfer_value() {
     console.log('Value: ', this.value_transfer);
     console.log('Destiny: ', this.destiny_transfer);
+    this.toTransfer.emit({valor: this.value_transfer, destino: this.destiny_transfer});
     console.log('Tranfer done!');
   }
 
