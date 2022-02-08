3691a335012cbfabe1da355430743df2be638896
Gabriel Jordão Alonso <gabrieljordaoalonso@gmail.com>
2022-02-08 14:52:05 -0300

    Insert FormModule in app.module

--- src/app/new-transfer/new-transfer.component.ts
+++ src/app/new-transfer/new-transfer.component.ts
@@ -1,11 +1,12 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector:'app-new-transfer',
-    templateUrl:'./new-transfer.component.html',
-    styleUrls:['./new-transfer.component.scss']
+  selector: 'app-new-transfer',
+  templateUrl: './new-transfer.component.html',
+  styleUrls: ['./new-transfer.component.scss'],
 })
-
 export class newTransferComponent {
-
+  transfer_value() {
+    console.log('Tranfer done!');
+  }
 }
