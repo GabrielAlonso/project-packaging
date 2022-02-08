45b01a44cd12a7945af11add13f66fb82706ea3b
Gabriel Jordão Alonso <gabrieljordaoalonso@gmail.com>
2022-02-08 14:41:32 -0300

    Implementation New Transfer screen

---
+++ src/app/new-transfer/new-transfer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-new-transfer',
+    templateUrl:'./new-transfer.component.html',
+    styleUrls:['./new-transfer.component.scss']
+})
+
+export class newTransferComponent {
+
+}
